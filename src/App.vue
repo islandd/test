7d6a387b8a0770f46161cf85767963983bab6709
zhangfangjie.iea <zhangfangjie.iea@bytedance.com>
2020-07-25 13:32:26 +0800

    fix-update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <h2>21212121eeeea</h2>
+     <p v-html="msg"></p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      msg:"<a href='http://www.baidu.com'></a>"
+    }
   }
 }
 </script>
